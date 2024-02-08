e5aec5828888de2819fae1412eac137729a3be30
tsuchipls <jdjneke@gmail.com>
2024-02-07 23:02:13 -0300

    first commit

---
+++ src/components/social/GoogleSignInButton.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount } from "svelte";
+  import GoogleIcon from "../icons/GoogleIcon.svelte";
+  import Spinner from "../icons/Spinner.svelte";
+  import { Button } from "../base/button";
+
+  let loading = false;
+
+  async function signIn() {
+    loading = true;
+    await handleGoogleSignIn();
+    loading = false;
+  }
+</script>
+
+<Button variant="google" on:click={signIn} disabled={loading}>
+  {#if loading}
+    <Spinner />
+    Please wait
+  {:else}
+    <GoogleIcon />
+    Sign in with Google
+  {/if}
+</Button>
